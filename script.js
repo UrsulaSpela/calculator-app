499c0683e983cc20d44f6ac717c174d380c73c5d
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-07-15 14:40:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-// let btn1 = parseInt(document.querySelector("#btn-one").innerText);
-// let btn2 = parseInt(document.querySelector("#btn-two").innerText);
-// let btn3 = parseInt(document.querySelector("#btn-three").innerText);
-// let btn4 = parseInt(document.querySelector("#btn-four").innerText);
-// let btn5 = parseInt(document.querySelector("#btn-five").innerText);
-// let btn6 = parseInt(document.querySelector("#btn-six").innerText);
-// let btn7 = parseInt(document.querySelector("#btn-seven").innerText);
-// let btn8 = parseInt(document.querySelector("#btn-eight").innerText);
-// let btn9 = parseInt(document.querySelector("#btn-nine").innerText);
-// let btn0 = parseInt(document.querySelector("#btn-zero").innerText);
-// let btnDel = document.querySelector("#btn-del").innerText;
-// let btnPlus = document.querySelector("#btn-plus").innerText;
-// let btnMinus = document.querySelector("#btn-minus").innerText;
-// let btnMultiply = document.querySelector("#btn-x").innerText;
-// let btnDivide = document.querySelector("#btn-slash").innerText;
-// let btnReset = document.querySelector("#btn-reset").innerText;
-// let btnEqual = document.querySelector("#btn-equals").innerText;
-// let btnDot = document.querySelector("#btn-dot").innerText;
+//  CALCULATOR
 
+// document.getElementsByTagName("button").addEventListener('click', displayValue);
+// function displayValue(){
+//     document.querySelector("#calculator-screen").innerHTML = document.querySelector("button").value;
+// }
 
 //  THEME SWITCHER
-
-//  CALCULATOR
