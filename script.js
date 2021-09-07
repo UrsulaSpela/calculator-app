21683f58a9d56b1a903071735a717f6fbed4793b
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-09-07 19:30:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-//  CALCULATOR
+// CALCULATOR
+//1. define buttons
+// let btn1 = document.querySelector("#btn-one")
+// let btn2 = document.querySelector("#btn-two")
+// let btn3 = document.querySelector("#btn-three")
+// let btn4 = document.querySelector("#btn-four")
+// let btn5 = document.querySelector("#btn-five")
+// let btn6 = document.querySelector("#btn-six")
+// let btn7 = document.querySelector("#btn-seven")
+// let btn8 = document.querySelector("#btn-eight")
+// let btn9 = document.querySelector("#btn-nine")
+// let btn0 = document.querySelector("#btn-zero")
 
-// document.getElementsByTagName("button").addEventListener('click', displayValue);
-// function displayValue(){
-//     document.querySelector("#calculator-screen").innerHTML = document.querySelector("button").value;
-// }
+// btn1 = 1
+// btn2 = 2
+// btn3 = 3
+// btn4 = 4
+// btn5 = 5
+// btn6 = 6
+// btn7 = 7
+// btn8 = 8
+// btn9 = 9
+// btn0 = 0
 
-//  THEME SWITCHER
+let numberBtn = document.querySelector(".nmbr")
+
+numberBtn.addEventListener("click", function(){
+    console.log(numberBtn.value)
+})
+
+//2. input , calculate, output
+
+
+
+// THEME SWITCHER
